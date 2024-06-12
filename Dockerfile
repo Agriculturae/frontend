d2a2c1a083703a1289099617d6d5a1ba379f1adc
Seyfullah <seyfullah.akgun@coens.io>
2024-06-12 10:51:37 +0300

    dcker config

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:16
 
 WORKDIR /app
 COPY package.json /app
+COPY vite.config.js /app
 RUN npm install
 COPY . /app
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  server: {
+    host: "0.0.0.0", // Tüm arabirimlerden dinle
+    port: 3000,
+  },
 });
